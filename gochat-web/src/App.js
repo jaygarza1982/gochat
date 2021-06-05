f6c7bd00a2c233154d382139b4b8b57e16319df4
Jake <jaygarza1982@gmail.com>
2021-06-04 22:22:05 -0400

    Added Tailwind CSS.

--- gochat-web/src/App.js
+++ gochat-web/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <div class="bg-gray-300 w-3/5 mx-4 my-2 p-2 rounded-lg">Tailwind test</div>
         <a
           className="App-link"
           href="https://reactjs.org"
